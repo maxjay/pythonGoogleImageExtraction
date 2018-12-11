d79c1f6c240f7ac9f7c44071da28df3010823f85
HappehBunneh <disgracedandbetrayed@gmail.com>
2018-12-11 10:20:22 +0000

    changed imports so whole modules arent imported

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # example input
 # extract.py -i one,two,three -b 2000 (optional) -o (optional)
 
-import argparse
-import urllib2
-import re
-import os
-import threading
-import time
-
+from argparse import ArgumentParser
+from urllib2 import Request, urlopen
+from re import findall
+from threading import Thread
+from time import sleep
+from os import path, makedirs
 class Fetch():
     
     RE_IMG_SRC = r'img .*?src="(.*?)"'
@@ -18,30 +17,11 @@ class Fetch():
         self.batchSize = batch if batch != None else 100
         self.outputLocation = output if output != None else ''
 
-    def run(self):
-        for i in self.terms:
-            for j in range(int(self.batchSize/100)):
-                url = 'https://www.google.com/search?tbm=isch&q='+i+'&ijn='+str(j+1)+'&start='+str(j+1)+'00&asearch=ichunk&async=_id:rg_s,_pms:s,_fmt:pc'
-                req = urllib2.Request(url, headers={'User-Agent' : 'Magic Browser'})
-                con = urllib2.urlopen(req)
-                htmlFile = con.read()
-                with open('f.txt', 'w') as f:
-                    f.write(htmlFile)
-                matches = re.findall(self.RE_IMG_SRC,htmlFile)
-                print len(matches)
-                string = self.outputLocation + i
-                if not os.path.isdir(string):
-                    os.makedirs(string)
-                for k in range(j*100,j*100+len(matches)):
-                    with open(string+'/'+str(k)+'.jpeg', 'wb+') as image:
-                        print matches[k-j*100]
-                        image.write(urllib2.urlopen(matches[k-(j*100)]).read())
-
     def runMainThread(self):
         for j in self.terms:
-            t = threading.Thread(target=self.runParentThread, args=(j,))
+            t = Thread(target=self.runParentThread, args=(j,))
             t.start()
-            time.sleep(0.01)
+            sleep(0.01)
     
     def runParentThread(self, term):
         if self.outputLocation != '':
@@ -49,31 +29,32 @@ class Fetch():
         else:
             string = self.outputLocation+term
         print string
-        if not os.path.isdir(string):
-            os.makedirs(string)
+        if not path.isdir(string):
+            makedirs(string)
         for i in range(self.batchSize//100):
             print i
-            t = threading.Thread(target=self.runChildThread, args=(term, i, string,))
+            t = Thread(target=self.runChildThread, args=(term, i, string,))
             t.start()
-            time.sleep(0.01)
+            sleep(0.01)
 
     def runChildThread(self, term, order, output):
         print 'Starting batch', order
         url = 'https://www.google.com/search?tbm=isch&q='+term+'&ijn='+str(order)+'&start='+str(order)+'00&asearch=ichunk&async=_id:rg_s,_pms:s,_fmt:pc'
         print url
-        req = urllib2.Request(url, headers={'User-Agent' : 'Dab on them Haters Browser'})
-        con = urllib2.urlopen(req)
+        req = Request(url, headers={'User-Agent' : 'Dab on them Haters Browser'})
+        con = urlopen(req)
         htmlFile = con.read()
-        matches = re.findall(self.RE_IMG_SRC, htmlFile)
+        matches = findall(self.RE_IMG_SRC, htmlFile)
         for k in range(order*100, order*100+len(matches)):
             if k%20 == 0:
                 print 'Batch', order, 'at', str(k-order*100)+'%'
             with open(output+'/'+str(k)+'.jpeg', 'wb+') as image:
-                image.write(urllib2.urlopen(matches[k-order*100]).read())
+                image.write(urlopen(matches[k-order*100]).read())
         print 'Finished batch', order
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract images from google based on search terms in batches')
+    parser = ArgumentParser(description='Extract images from google based on search terms in batches')
     parser.add_argument('-i','--input', nargs='+', help='Search terms for images you want to find', required=True, type=str)
     parser.add_argument('-b','--batchsize', help='Number of images to fetch for every term (default: 100)', required=False, type=int)
     parser.add_argument('-o', '--output', help='Folder to output to (default: output)', required=False, type=str)
